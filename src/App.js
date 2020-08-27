a799361c95919febf615af2f2a7dcab248cdf948
zubi <zubayerahmed232@gmail.com>
2020-08-27 15:58:39 +0600

    file structure changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react'
-import Dogs from './Dogs'
-import Navigbar from './components/Navigbar';
-import Course from './components/Course';
+import React from "react";
+import Navigbar from "./components/Navigbar";
+import Course from "./components/Course";
 
 export default function App() {
-  return (
-    <div className="app">
-      
-        <Navigbar/>
-     
-        <Course/>
-      
-    </div>
-  )
+	return (
+		<div className="app">
+			<Navigbar />
+			<Course />
+		</div>
+	);
 }
-
-
-
-
-
-
-
-
